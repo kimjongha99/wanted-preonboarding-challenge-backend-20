077625f0a55c6f50a5bf9e94057a7e9dd96a5329
kimjongha99 <jjhsun99@gmail.com>
2024-06-09 23:45:55 +0900

    Feat: 초기 설정 및 RDB구성

---
+++ wanted-challenge/src/test/java/com/example/wantedchallenge/WantedChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wantedchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
